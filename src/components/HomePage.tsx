1b70c2e2ada53c4e9845e8960375926dbd2ae954
Daniyar Bashirov <tehnikdaniyar@gmail.com>
2024-08-02 14:23:21 +0600

    feat: create section help

---
+++ src/components/HomePage.tsx
@@ -0,0 +1,28 @@
+import react from "react";
+import { } from 'react-router-dom'
+
+
+export default function HomePage() {
+
+	const res = (8 / 2 + 5 - -3 / 2);
+	console.log(res);
+
+	// (function Res() {
+
+	// })()
+	/* */
+	const template = {
+		'1': 'one',
+
+	}
+
+	console.log(template['1']);
+
+	return (
+		<main style={{ maxWidth: 1200, padding: 20 }}>
+
+		</main>
+	)
+}
+
+
